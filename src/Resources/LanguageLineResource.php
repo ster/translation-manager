87df9459335224d34858120f2d2167791e32b792
Stefano Mazzega <stefano.mazzega@gmail.com>
2026-04-08 12:05:48 +0200

    Fix EditAction namespace: moved from Tables\Actions to Actions in Filament 5

--- src/Resources/LanguageLineResource.php
+++ src/Resources/LanguageLineResource.php
@@ -10,7 +10,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\ViewField;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
-use Filament\Tables\Actions\EditAction;
+use Filament\Actions\EditAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ViewColumn;
